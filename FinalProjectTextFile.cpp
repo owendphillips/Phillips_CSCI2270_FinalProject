28b4098ae991313ae8b7496528b4bf7d32b2eb37
owendphillips <30354572+owendphillips@users.noreply.github.com>
2017-07-27 18:27:13 -0600

    Create FinalProjectTextFile.cpp

---
+++ FinalProjectTextFile.cpp
@@ -0,0 +1,25 @@
+lawnmower, 1100, Gardening
+t-shirt, 20, Clothing
+cd, 10, Music
+pants, 25, Clothing
+socks, 15, Clothing
+beanie, 15, Clothing
+baseball cap, 18, Clothing
+dictionary, 40, Books
+book, 30, Books
+skateboard, 140, Sports
+bicycle, 500, Sports
+laptop, 850, Electronics
+sunglasses, 20, Clothing
+clock, 40, Furniture
+desk, 250, Furniture
+chair, 300, Furniture
+couch, 400, Furniture
+vase, 25, Furniture
+mirror, 30, Furniture
+vinyl lp, 15, Music
+stereo, 600, Electronics
+headphones, 120, Electronics
+trading cards, 10, Toys
+teddy bear, 20, toys
+yo-yo, 22, toys
